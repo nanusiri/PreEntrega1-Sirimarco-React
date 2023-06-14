000619819e7e166ddf54c773db3b05f9497eae43
Nahuel <nanualejandro@gmail.com>
2023-06-14 19:44:56 -0300

    intento exitoso de unificacion

---
+++ src/components/Items.jsx
@@ -0,0 +1,34 @@
+export const fetchedItems = [
+    {
+        id: 1,
+        nombre: "BEEF JERKY: Original",
+        precio: 700,
+        stock: 10,
+        img: "https://cdn.shopify.com/s/files/1/0186/5049/7124/products/1000000924280gJLBFORIGJERKCAHERO.png?v=1620250955",
+        categoryId: 1
+    },
+    {
+        id: 2,
+        nombre: "BEEF JERKY: Sweet & Hot",
+        precio: 800,
+        stock: 0,
+        img: "https://images.albertsons-media.com/is/image/ABS/960130428-C1N1?$ng-ecom-pdp-mobile$&defaultImage=Not_Available",
+        categoryId: 2
+    },
+    {
+        id: 3,
+        nombre: "BEEF JERKY: Teriyaki",
+        precio: 750,
+        stock: 5,
+        img: "https://www.jacklinks.com/shop/media/catalog/product/1/0/10000017985_3.25oz_jl_bf_teri_jerk_hero_2.png",
+        categoryId: 3
+    },
+    {
+        id:4,
+        nombre: "BEEF JERKY: Peppered",
+        precio: 750,
+        stock: 8,
+        img: "https://www.ubuy.com.ar/productimg/?image=aHR0cHM6Ly9tLm1lZGlhLWFtYXpvbi5jb20vaW1hZ2VzL0kvOTFtNDYxeDRkakwuX1NMMTUwMF8uanBn.jpg",
+        categoryId: 3
+    }
+]
