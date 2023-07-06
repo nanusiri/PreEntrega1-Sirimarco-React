eb8834f9d42540fc752d0aa6df7d311847737c16
Nahuel <nanualejandro@gmail.com>
2023-07-06 20:32:26 -0300

    Tengo el context agregado pero no me anda la funcion para agregar items a cartList

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -3,17 +3,17 @@ import ItemCount from "./ItemCount"
 import { CartContext } from "../context/CartContext"
 
 const ItemDetail = ({item}) => {
+    const [showItemCount, setShowItemCount] = useState(true)
+    const [selectedQuantity, setSelectedQuantity] = useState(0)
     const {addToCart} = useContext(CartContext)
 
     const handleAddToCart = (count) => {
-        addToCart(...item, 1)
+        setSelectedQuantity(count)
+        setShowItemCount(false)
+        addToCart(item)
       }
 
     const handleFinishPurchase = () => {
-        // Aquí puedes implementar la lógica para finalizar la compra, como agregar el ítem al carrito
-        // y redirigir al usuario a la página de carrito ('/cart').
-    
-        // Por ahora, simplemente redireccionamos al usuario a la página vacía de carrito ('/cart').
         window.location.href = "/cart";
     }
 
@@ -23,11 +23,17 @@ const ItemDetail = ({item}) => {
         <img src={item.img} alt={item.nombre} />
         <p>Precio: ${item.precio}</p>
         <p>Descripcion: Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, ratione? Commodi illo magnam vitae non perspiciatis, iusto iste, voluptate fugiat corporis ad dolor vero harum necessitatibus accusamus. Quibusdam, aliquam in. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi fuga provident mollitia quod inventore illo veniam earum voluptates delectus facere optio, ut dicta laudantium ea voluptate placeat. Maiores, iusto officia.</p>
+        {showItemCount ? (
         <ItemCount stock={item.stock} onAdd={handleAddToCart} />
-      <button onClick={handleFinishPurchase}>Terminar mi compra</button>
+      ) : (
+        <>
+          <p>Cantidad seleccionada: {selectedQuantity}</p>
+          <button onClick={handleFinishPurchase}>Terminar mi compra</button>
+        </>
+      )}
       <p>Stock: {item.stock}</p>
     </div>
-  );
-};
+    )
+}
 
-export default ItemDetail;
+export default ItemDetail
